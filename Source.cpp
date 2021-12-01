34825b2c6afa6f3ce2873332713f542ca733c68f
JustinStri <jstrikowski@ufl.edu>
2021-11-30 20:45:20 -0500

    this is a test. All the basic objects and classes have been contructed

--- Source.cpp
+++ Source.cpp
@@ -1,6 +1,72 @@
-#include "Header.h"
+#include "Header1.h"
+#include <vector>
+#include <map>
+#include <vector>
+#include <string>
+#include <iostream>
+using namespace std;
+
+class Sugar
+{
+public:
+	string name;
+	Sugar() {}
+	~Sugar() {}
+
+private:
+
+};
+
+class Food
+{
+public:
+	int ID;
+	string name;
+	string ingredients;
+
+	Food() {}
+	Food(int ID, string name) {
+		this->ID = ID;
+		this->name = name;
+	}
+	~Food() {}
+
+private:
+
+};
+
+class EdgeList
+{
+public:
+	//pair between the food and the sugar
+	vector<pair<Food*, Sugar*>> theList;
+	EdgeList() {}
+	~EdgeList() {}
+	
+
+private:
+	
+};
+
+class AdjacencyMatrix
+{
+public:
+
+	/*int is the ID, the vector is the complete list of sugars, and when the sugar is in the list of ingredients 
+	the number is either 1 (if the graph is unwieghted) or the weight if it is weighted*/
+	map<int, vector<int>> theMatrix; 
+	AdjacencyMatrix() {}
+	~AdjacencyMatrix() {}
+
+private:
+
+};
 
 int main() {
+
+	Food cheeto = Food(1234, "cheetos");
+
 	cout << "Hello world!" << endl;
+	cout << cheeto.ID << " " << cheeto.name << endl;
 	return 0;
 }
